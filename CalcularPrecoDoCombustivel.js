3141a63b16b40505a269f4e8243c4dd23ff06ae9
unknown <guimaraesgn30@gmail.com>
2022-10-27 16:21:17 -0300

    Alteração no preço do combustivel

--- CalcularPrecoDoCombustivel.js
+++ CalcularPrecoDoCombustivel.js
@@ -1,18 +1,23 @@
 console.log('\n---Programa para Calcular sua Viagem com o Preço atual do Combustivel---\n')
 
-let precoDoCombustivel = 5.79;
-
+let precoDoEtanol = 5.79;
+let precoDoGasolina = 6.66;
 const valorMedioCarroPorKm = 10;
-
 let distanciaDaViagemKm = 100;
+const tipoCombustivel = 'Gasolina';
 
 let gasolinaporKm = distanciaDaViagemKm / valorMedioCarroPorKm;
 
-console.log('Gastos em uma viagem: ');
+if (tipoCombustivel === 'Etanol') {
+    let gastoTotal = gasolinaporKm * precoDoEtanol;
+    console.log('Gastos em uma viagem: ');
+    console.log(gastoTotal.toFixed(2));
+} else {
+    let gastoTotal = gasolinaporKm * precoDoGasolina;
+    console.log('Gastos em uma viagem: ');
+    console.log(gastoTotal.toFixed(2));
+}
 
-let gastoTotal = gasolinaporKm * precoDoCombustivel;
-
-console.log(gastoTotal.toFixed(2));
 
 
 
